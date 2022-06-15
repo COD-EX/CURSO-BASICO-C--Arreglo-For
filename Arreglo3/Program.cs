13db529dcd54346265d5f7ed3d9105e1034be071
alin piero <alinpiero@gmail.com>
2022-06-15 00:25:00 -0500

    Arreglo con For

---
+++ Arreglo3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arreglo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] valores = new int[5];
+            int i;
+            int suma=0;
+            for(i = 0; i <valores.Length; i++)
+            {
+                //Console.WriteLine("Posicion " + i);
+                Console.WriteLine("Ingrese valores en la posición: "+i);
+                string N = Console.ReadLine();
+                //int M = Convert.ToInt32(N);
+                valores[i] = Convert.ToInt32(N);
+                
+                suma=suma+valores[i];
+            }
+            for (i=0;i<valores.Length;i++)
+            {
+                Console.WriteLine("Valores ingresado es " + valores[i]);
+            }
+            Console.WriteLine("Suma total es: " + suma);
+            Console.ReadKey();
+            
+        }
+       
+    }
+}
